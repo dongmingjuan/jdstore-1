6517ac9da8c54ec9659ed9f42fc3120ef5946ae6
dongmingjuan <d387918156@gmail.com>
2017-08-22 10:25:10 +0800

    admin can upload product and only admin can do it

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'welcome#index'
+  root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :products
+  namespace :admin do
+    resources :products
+  end
 end
